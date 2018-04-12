b49414a83d7dddf0037c1e4b378b52a628ac75d9
james1708 <37161983+james1708@users.noreply.github.com>
2018-04-12 12:26:14 +1000

    Fixing conversions from VB

---
+++ EndingGameController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SwinGameSDK; //checked by David, all fine
+
+namespace BattleShip
+{
+
+    static class EndingGameController
+    {
+        public static void DrawEndOfGame ()
+        {
+            Rectangle toDraw = new Rectangle ();
+            string whatShouldIPrint;
+
+            UtilityFunctions.DrawField (GameController.ComputerPlayer.PlayerGrid, GameController.ComputerPlayer, true);
+            UtilityFunctions.DrawSmallField (GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
+
+            toDraw.X = 0;
+            toDraw.Y = 250;
+            toDraw.Width = SwinGame.ScreenWidth ();
+            toDraw.Height = SwinGame.ScreenHeight ();
+
+            if (GameController.HumanPlayer.IsDestroyed) {
+                whatShouldIPrint = "YOU LOSE!";
+            } else {
+                whatShouldIPrint = "-- WINNER --";
+            }
+
+            SwinGame.DrawTextLines(whatShouldIPrint, Color.White, Color.Transparent, GameResources.GameFont ("ArialLarge"), FontAlignment.AlignCenter, toDraw);
+        }
+
+        public static void HandleEndOfGameInput ()
+        {
+            if (SwinGame.MouseClicked (MouseButton.LeftButton) || SwinGame.KeyTyped (KeyCode.ReturnKey) || SwinGame.KeyTyped (KeyCode.EscapeKey)) {
+                HighScoreController.ReadHighScore (GameController.HumanPlayer.Score);
+                GameController.EndCurrentState ();
+            }
+        }
+    }
+}
